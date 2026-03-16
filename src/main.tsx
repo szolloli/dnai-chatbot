3de4533262e46d7466bc37592bfe0f4c49ff82c7
szolloli <szolloli@fit.cvut.cz>
2026-03-16 17:18:19 +0200

    chore: remove redundant css files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
 import App from "./App.tsx";
-import "./index.css";
 import { appTheme } from "./theme/appTheme";
 
 createRoot(document.getElementById("root")!).render(
